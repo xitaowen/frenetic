05fc468644eede9724986735c3d8d2254d11b5d8
Arjun Guha <arjun@cs.umass.edu>
2013-10-16 20:01:54 -0400

    sketch of template NetKAT syntax

---
+++ lib/TemplateSyntax.mli
@@ -0,0 +1,28 @@
+type pos = Lexing.position
+
+type id = string
+
+type header = NetKAT_Types.header
+
+type header_val = NetKAT_Types.header_val
+
+type exp =
+  | Id of pos * id
+  | Let of pos * id * exp * exp
+  | Fun of pos * id list * exp
+  | App of pos * exp * exp list
+  | If of pos * exp * exp * exp
+  | Par of pos * exp * exp
+  | Seq of pos * exp * exp
+  | Mod of pos * exp * exp
+  | Filter of exp
+  | True
+  | False
+  | Test of exp * exp
+  | And of exp * exp
+  | Or of exp * exp
+  | Neg of exp
+  | Header of header
+  | HeaderVal of header_val
+
+val eval : exp -> NetKAT_Types.policy
