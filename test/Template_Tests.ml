0bb02d28d730db43fcc548a8e2f5bb8118b4f9d6
Arjun Guha <arjun.guha@gmail.com>
2013-11-05 11:42:45 -0500

    type-checking stuff in examples/templates

--- test/Template_Tests.ml
+++ test/Template_Tests.ml
@@ -3,7 +3,19 @@ open TemplateSyntax
 let template_parse (t : string) : exp = 
   Template_Parser.program Template_Lexer.token (Lexing.from_string t)
 
+let type_check_file (filename) =
+    let cin = open_in filename in
+    let template_exp = Template_Parser.program Template_Lexer.token (Lexing.from_channel cin)
+    in TemplateTypeChecker.type_check template_exp TemplateSyntax.TPol
 
 
 let p1 = "(fun x : pol : pol = x) (filter true)"
+
 TEST "Typeis test1" =  (eval (template_parse p1)) = Types.Filter Types.True
+
+
+TEST "repeater.kat" = type_check_file ("examples/templates/repeater.kat")
+
+TEST "Chapter7.kat" = type_check_file ("examples/templates/Chapter7.kat")
+
+TEST "Chapter8.kat" = type_check_file ("examples/templates/Chapter8.kat")
